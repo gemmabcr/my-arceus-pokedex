1640a6575fd3271942bea54c1720881076416595
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-12-02 18:56:35 +0100

    feat: sorted my team cards

--- src/components/MyTeamSection/PokemonLine/PokemonLine.js
+++ src/components/MyTeamSection/PokemonLine/PokemonLine.js
@@ -4,10 +4,11 @@ import {FlexColumn} from '../../../commonStyled'
 
 const PokemonLine = ({hisuiPokedex, setHisuiPokedex, myTeam }) => {
   const renderTeam = myTeam.map(index => hisuiPokedex.find(pokemon => pokemon.index === index))
+  const sortedTeam = renderTeam.sort((a, b) => a.index - b.index)
 
   return (
     <FlexColumn>
-      {renderTeam.length > 0 && renderTeam.map(pokemon => {
+      {renderTeam.length > 0 && sortedTeam.map(pokemon => {
         return (
           <PokemonCard
             key={pokemon.index}
